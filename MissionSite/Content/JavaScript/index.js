c4d7e4f56961c1789ae21c8e68049fc22d2a7ca6
DevinBennett <dbennettksm58@gmail.com>
2016-10-31 09:45:13 -0600

    bunha commits

---
+++ MissionSite/Content/JavaScript/index.js
@@ -0,0 +1,20 @@
+﻿
+    $(document).ready(function () {
+        $("#reply").click(function () {
+            $(".replyBox").toggle();
+        });
+    });
+
+
+    $(document).ready(function () {
+        $("#reply2").click(function () {
+            $(".replyBox2").toggle();
+        });
+    });
+    
+    $(document).ready(function () {
+        $("#reply3").click(function () {
+            $(".replyBox3").toggle();
+        });
+    });
+      
